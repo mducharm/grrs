0d1de7f5b4136fb32658a21212b005e3291c2fc4
Michael <sheveksmath@gmail.com>
2019-10-05 18:16:18 -0400

    instantiated Cli struct

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ use structopt::StructOpt;
 #[derive(StructOpt)]
 struct Cli {
     pattern: String,
+    #[structopt(parse(from_os_str))]
     path: std::path::PathBuf,
 }
 
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
+    let args = Cli::from::args();
 }
